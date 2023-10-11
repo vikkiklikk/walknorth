4c8515aecf7bab4869f2968e09d8c4a87195bb4e
Viktor Birgisson <108062355+vikkiklikk@users.noreply.github.com>
2023-10-11 16:50:47 +0000

    Initial commit with base folders and files

---
+++ src/main.js
@@ -0,0 +1,4 @@
+var message = 'Hello World';
+console.log(message);
+var message2 = 'Hello World number two';
+console.log(message2);
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+let message: string = 'Hello World';
+console.log(message);
+
+let message2: string = 'Hello World number two';
+console.log(message2);
