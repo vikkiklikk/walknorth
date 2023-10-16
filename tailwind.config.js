d94607c9cd163a59604bf069aaee73534348d732
BjorkBjorgulfs <bogga1387@gmail.com>
2023-10-16 23:34:52 +0000

    deleted colors that I made earlier

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'text-color': '#353535',
-        'light-color': '#D8E0E9',
-        'blue-color': '#56729B',
-        'red-button': '#A22E3E',
       },
     },
   },
