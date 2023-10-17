ef00d7f2262e69cc38232ddd73153aac54f249c1
BjorkBjorgulfs <bogga1387@gmail.com>
2023-10-17 11:09:28 +0000

    styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
       colors: {
         customBlue1: '#56729B',
         customBlue2: '#D8E0E9',
+        customFontColor: '#353535',
+        customLogoText: '#274D83',
       },
     },
   },
