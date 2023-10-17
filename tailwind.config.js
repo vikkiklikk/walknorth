3f0ca2c52786c5e8183e79b82f05b4cab33aca3b
BjorkBjorgulfs <bogga1387@gmail.com>
2023-10-17 00:19:33 +0000

    trying to fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,32 +4,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        customBlue1: '#56729B',
+        customBlue2: '#D8E0E9',
       },
     },
   },
   plugins: [],
 }
-
-// colour for the head/footer
-
-module.exports = {
-  theme: {
-    extend: {
-      colors: {
-        customBlue1: '#56729B',
-      },
-    },
-  },
-}
-// colour for the background
-module.exports = {
-  theme: {
-    extend: {
-      colors: {
-        customBlue2: '#D8E0E9',
-      },
-    },
-  },
-}
-
-
