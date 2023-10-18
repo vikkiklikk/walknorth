8f6a906de22bc33d5a4b1a0a8d3b1de01acc3684
McgrathAaron <135990095+McgrathAaron@users.noreply.github.com>
2023-10-18 16:38:00 +0000

    Keilir info css (#6)
    * starting out
    
    * I put all the things into the html file
    
    * update my tailwind, it made change to output.css
    
    * deleted colors that I made earlier
    
    * changed icons
    
    changed icons
    
    * trying to fix tailwind
    
    * changed some icons
    
    * styling
    
    * typescript for "favorite button"
    
    * trying to fix filled star when toggle
    
    * new branch with CSS, not tailwind
    
    * finishing styling
    
    * changed font family
    
    * made html for the other trails, fixed star toggle
    
    * connecting trail sites to the index
    
    * changed images
    
    * styling details
    
    * deleted old index
    
    * Update burgermenu
    
    ---------
    
    Co-authored-by: BjorkBjorgulfs <bogga1387@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,124 @@
-let message: string = 'Hello World';
-console.log(message);
 
-let message2: string = 'Hello World number two';
-console.log(message2);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const favoriteButton = document.getElementById('favoriteButton') as HTMLButtonElement | null;
+const emptyStar = document.getElementById('emptyStar') as HTMLImageElement | null;
+const filledStar = document.getElementById('filledStar') as HTMLImageElement | null;
+
+if (favoriteButton && emptyStar && filledStar) {
+  favoriteButton.addEventListener('click', function() {
+    // Check whether the filled star is currently displayed.
+    const isFavorited = getComputedStyle(filledStar).display === "block";
+
+    if (isFavorited) {
+      // If the filled star is visible, hide it and show the empty star.
+      filledStar.style.display = 'none';
+      emptyStar.style.display = 'block';
+    } else {
+      // If the filled star is not visible (i.e., the empty star is visible), hide the empty star and show the filled star.
+      filledStar.style.display = 'block';
+      emptyStar.style.display = 'none';
+    }
+
+    // Here, you can also add code for any additional styling or functionality you want to trigger on this action.
+  });
+} else {
+  console.error('One or more elements were not found!');
+}
 
